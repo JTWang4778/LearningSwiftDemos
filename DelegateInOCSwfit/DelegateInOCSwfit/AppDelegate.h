9b0bd2216bf81dcaa4f051e2d0fd2f7047fd31c3
JTWang <JTWang@sina.com>
2017-05-31 22:03:03 +0800

    不知道是神

---
+++ DelegateInOCSwfit/DelegateInOCSwfit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DelegateInOCSwfit
+//
+//  Created by 王锦涛 on 2017/5/17.
+//  Copyright © 2017年 JTWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DelegateInOCSwfit/DelegateInOCSwfit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelegateInOCSwfit
+//
+//  Created by 王锦涛 on 2017/5/17.
+//  Copyright © 2017年 JTWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
